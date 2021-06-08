488b90fa5a1856f584adff11e7b2c9ba76c7b199
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-06-08 10:28:25 +0200

    add github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import "../stylesheets/App.css";
 import data from "../data/data.json";
 import PokeList from "./PokeList";
-console.log(data);
+import image from "../images/image.png";
 function App() {
   return (
     <div className="pokemon">
-      <h1 className="title">Mi lista de Pokemon</h1>
+      <div className="header">
+        <h1 className="title">Mi lista de</h1>
+        <img className="logo" src={image} />
+      </div>
       <PokeList pokedata={data} />
     </div>
   );
